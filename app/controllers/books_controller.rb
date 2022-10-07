fa567cb72bc15073eee2337683a39f259a9b70ef
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-10-07 19:53:51 +0900

    indexページ作成中

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,8 +1,13 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
   
   def create
+    @book = Book.new(book_params)
+    if @book.save
+      redirect_to book_path(@book.id)
+    end
   end
   
   def index
@@ -19,5 +24,11 @@ class BooksController < ApplicationController
   end
   
   def destroy
+  end
+  
+  private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
