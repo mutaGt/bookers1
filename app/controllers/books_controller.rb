6db48071d81442308794061665f4f2df7813c595
mutaGt <104976989+mutaGt@users.noreply.github.com>
2022-10-14 16:09:53 +0900

    本の作成時のバリデーション

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -4,14 +4,18 @@ class BooksController < ApplicationController
   end
   
   def create
+    @books = Book.all
     @book = Book.new(book_params)
     if @book.save
       redirect_to book_path(@book.id)
+    else
+      render :index
     end
   end
   
   def index
     @books = Book.all
+    @book = Book.new
   end
   
   def show
@@ -24,8 +28,11 @@ class BooksController < ApplicationController
   
   def update
     @book = Book.find(params[:id])
-    @book.update(book_params)
-    redirect_to book_path(@book.id)
+    if @book.update(book_params)
+      redirect_to book_path(@book.id)
+    else
+      render :edit
+    end
   end
   
   def destroy
